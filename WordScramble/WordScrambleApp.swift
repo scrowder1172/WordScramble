fa92f4c9488229d1f840d7da755adfa9cba8d8b7
Scott Crowder <scrowder@gmail.com>
2023-10-23 12:03:09 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by SCOTT CROWDER on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
